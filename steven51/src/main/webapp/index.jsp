40255870c7cfe092780f08ca539dc4c3ddd8035f
XC-20161107HDJY\Administrator <3170886904@qq.com>
2018-10-08 23:22:50 +0800

    修改

--- steven51/src/main/webapp/index.jsp
+++
@@ -1,5 +0,0 @@
-<html>
-<body>
-<h2>Hello World!haha</h2>
-</body>
-</html>
--- steven51/src/mian/java/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package dao;
-
-import entity.User;
-
-public interface UserDao {
-	public boolean regist(User user);
-	public boolean update(User user);
-	
-}
